03af67c7641bf4708e3fd7c3ec294d9e577e77d2
Douglass Turner <douglass.turner@gmail.com>
2022-10-03 17:27:14 -0400

    Bug fix. Point cloud data conversion (#6)

--- src/pointCloudConverter.js
+++ src/pointCloudConverter.js
@@ -4,60 +4,35 @@ class PointCloudConverter {
 
     convert({ path, chr, genome, name, string }) {
 
-        let raw = string.split(/\r?\n/);
+        let raw = string.split(/\r?\n/)
 
         // discard blank lines
-        let lines = raw.filter(line => "" !== line);
+        let lines = raw.filter(line => "" !== line)
 
         // discard initial on-liner
-        lines.shift();
+        lines.shift()
 
-        console.time(`convertion of point-cloud file ${ path } with ${ lines.length } points`);
+        console.time(`convertion of point-cloud file ${ path } with ${ lines.length } points`)
 
-        let hash = {};
-        for (let line of lines) {
+        const output = []
+        output.push(`##format=sw1 name=${ name } genome=${ genome }`)
 
-            const tokens = line.split(',');
-            const [ startBP, endBP, sizeKB, x, y, z ] = [ tokens[ 0 ], tokens[ 1 ], tokens[ 2 ], tokens[ 3 ], tokens[ 4 ], tokens[ 5 ] ];
+        const column_headings = []
+        column_headings.push('chromosome', 'start', 'end', 'x', 'y', 'z')
+        output.push(column_headings.join('\t'))
 
-            const key = `${ startBP }%${ endBP }%${ sizeKB }`;
-            if (undefined === hash[ key ]) {
-                hash[ key ] = [];
-            }
+        // Assume the file is a single pointcloud trace
+        // TODO: How to handle ensemble of pointcloud traces
+        output.push(`trace 0`)
 
-            hash[ key ].push({ startBP, endBP, x, y, z });
-
+        for (const line of lines) {
+            const [ startBP, endBP, sizeKB, x, y, z ] = line.split(',')
+            output.push(`${ chr } ${ startBP } ${ endBP } ${ x } ${ y } ${ z }`)
         }
 
-        let output = [];
-        output.push(`##format=sw1 name=${ name } genome=${ genome }`);
+        console.timeEnd(`convertion of point-cloud file ${ path } with ${ lines.length } points`)
 
-        let column_headings = [];
-        column_headings.push('chromosome');
-        column_headings.push('start');
-        column_headings.push('end');
-        column_headings.push('x');
-        column_headings.push('y');
-        column_headings.push('z');
-
-        output.push(column_headings.join('\t'));
-
-        let keys = Object.keys(hash);
-        for (let key of keys) {
-
-            output.push(`trace ${ keys.indexOf(key)}`);
-            const trace = hash[ key ];
-
-            for (let row of trace) {
-                const { startBP, endBP, x, y, z } = row;
-                output.push(`${ chr } ${ startBP } ${ endBP } ${ x } ${ y } ${ z }`)
-            }
-
-        }
-
-        console.timeEnd(`convertion of point-cloud file ${ path } with ${ lines.length } points`);
-
-        return output.join('\n');
+        return output.join('\n')
 
     }
 
